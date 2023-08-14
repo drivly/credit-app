0b416baea87162ee471cc8328b89710c8e1b01a2
Chris Risner <crisner.prsvr@gmail.com>
2023-08-14 11:39:00 -0500

    update setup react-query

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,12 +1,25 @@
 'use client'
 
 import React from 'react'
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import getQueryClient from './getQueryClient'
 
-const Providers = ({ children }: { children: React.ReactNode}) => {
-  const queryClient = getQueryClient()
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = React.useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry(failureCount, error: any) {
+            if (error.status === 404) return false
+            else if (failureCount < 2) return true
+            else return false
+          },
+        },
+      },
+    })
+  )
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
